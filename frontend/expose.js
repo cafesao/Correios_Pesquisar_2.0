3eb087c8d72b3de12b18c0a8025699aafb94fbb5
GabrielDiasDutra <gabriel.dutra@ezops.com.br>
2021-02-25 18:15:21 -0300

    modify 2

---
+++ frontend/expose.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+app.use(express.static('build'))
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
